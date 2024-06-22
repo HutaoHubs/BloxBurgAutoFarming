cbc44084afc092fb6f4d134d80062bcc86cffbc5
HutaoHubs <chacagaming011@gmail.com>
2024-06-22 23:18:40 +0700

    Update and rename README.md to Autofarm.lua

---
+++ Autofarm.lua
@@ -0,0 +1,3 @@
+ScriptJob = "HairDresser" 
+ScriptKey = "95a31a8935aca5f92b840d22892124cd"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HutaoHubs/Bloxburg/main/Bloxburgautofarm.lua"))();
